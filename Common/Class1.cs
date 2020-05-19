51c6e27e478622d4cda5a439c6047df0bf21c0f5
Ottavio Campana <o.campana@videotec.com>
2020-05-19 16:00:43 +0200

    initial commit

---
+++ Common/Class1.cs
@@ -0,0 +1,16 @@
+﻿using LibVLCSharp.Shared;
+
+namespace Common
+{
+    public class Class1
+    {
+        private readonly LibVLC libvlc;
+
+        Class1 ()
+        {
+            Core.Initialize();
+
+            libvlc = new LibVLC();
+        }
+    }
+}
